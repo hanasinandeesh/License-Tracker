af664de3dbe6d2528496d41dbbc1c130375c98dd
Hanasi Nandeesh <152947589+hanasinandeesh@users.noreply.github.com>
2026-01-19 13:13:22 +0530

    Update LicenseRepository.java

--- backend/src/main/java/com/prodapt/license_tracker/license/repository/LicenseRepository.java
+++ backend/src/main/java/com/prodapt/license_tracker/license/repository/LicenseRepository.java
@@ -27,21 +27,6 @@ public interface LicenseRepository extends JpaRepository<License, String> {
 			""")
 	List<License> findByValidToBefore(LocalDate date);
 
-//	@Query(value = """
-//			    SELECT
-//			        l.license_key AS licenseKey,
-//			        l.software_name AS softwareName,
-//			        COALESCE(v.vendor_name, 'N/A') AS vendorName,
-//			        COUNT(a.assignment_id) AS devicesUsed,
-//			        l.valid_to AS validTo
-//			    FROM license l
-//			    LEFT JOIN vendor v ON v.vendor_id = l.vendor_id
-//			    LEFT JOIN assignment a ON a.license_key = l.license_key
-//			    WHERE l.valid_to BETWEEN :from AND :to
-//			    GROUP BY l.license_key, l.software_name, v.vendor_name, l.valid_to
-//			""", nativeQuery = true)
-//	List<LicenseExpiryView> findExpiringLicenseView(@Param("from") LocalDate from, @Param("to") LocalDate to);
-
 	@Query(value = """
 			    SELECT
 			        l.license_key AS licenseKey,
